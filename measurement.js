c5431c622c1354200ee5dc39dbd49ea4b368625a
Hervé Ménager <herve.menager@gmail.com>
2015-06-16 21:31:12 +0200

    add alternative models for measurement data.

--- measurement.js
+++ measurement.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var _ = require('underscore');
+var exports = {};
 
 var MeasurementSchema = mongoose.Schema({
       "measId": String,
@@ -9,4 +10,21 @@ var MeasurementSchema = mongoose.Schema({
 
 var Measurement = mongoose.model('Measurement', MeasurementSchema);
 
-module.exports = Measurement;
+var MeasurementExpSchema = mongoose.Schema({
+      "measId": String,
+      "expId": String,
+      "values": [Number]});
+
+var MeasurementExp = mongoose.model('MeasurementExp', MeasurementExpSchema);
+
+var MeasurementGeneSchema = mongoose.Schema({
+      "measId": String,
+      "geneId": String,
+      "values": [Number]});
+
+var MeasurementGene = mongoose.model('MeasurementGene', MeasurementGeneSchema);
+
+
+module.exports = {measurement: Measurement,
+                  measurementExp: MeasurementExp,
+                  measurementGene: MeasurementGene};
