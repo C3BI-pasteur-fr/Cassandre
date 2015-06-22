a2477e55e10da47a97692a441f181911a0c5d6f1
Hervé Ménager <herve.menager@gmail.com>
2015-06-22 15:36:47 +0200

    add basic server methods to get data from measurements collection
    important: remember to correctly encode measurement, experiment and
    gene IDs, e.g.:
    http://localhost:8080/api/measurements/example_data%2Fscreens_data_05-07-2013_17h52m08s.txt/exp/pUM497_gal%2C%20mut%20exoY%20TAP%20-%20%20C130523_Haplo_pUM497Gal%20-%202013-06-3
    or
    http://localhost:8080/api/measurements/example_data%2Fscreens_data_05-07-2013_17h52m08s.txt/gene/YPR201W

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 var express = require("express");
+var mongoose = require("mongoose");
+
+var Measurement = require("./measurement").measurement;
 
 // ----- Configuration -------------------------------------------------- //
 
@@ -13,6 +16,40 @@ var pid = process.pid;
 app.use(express.static(__dirname + "/public"));             // Setting static content directory
 
 // ----- Routing -------------------------------------------------------- //
+
+mongoose.connect('mongodb://localhost/cassandre');
+
+/* list all the contents of the measurements collection  - extremely slow, should it be removed? */
+app.get('/api/measurements/', function(req, res, next) {
+  Measurement.find(
+    function (err, list) {
+      if (err) throw err;
+      res.json(list);
+  });
+});
+
+/* list all the values for a given experiment in a given measurement */
+app.get('/api/measurements/:mId/exp/:expId', function(req, res, next) {
+  Measurement.find({
+      'measId': req.params.mId,
+      'expId': req.params.expId
+    },function (err, list) {
+      if (err) throw err;
+      res.json(list);
+  });
+});
+
+/* list all the values for a given gene in a given measurement */
+app.get('/api/measurements/:mId/gene/:geneId', function(req, res, next) {
+  Measurement.find({
+      'measId': req.params.mId,
+      'geneId': req.params.geneId
+    },function (err, list) {
+      if (err) throw err;
+      res.json(list);
+  });
+});
+
 
 
 // ----- Server --------------------------------------------------------- //
