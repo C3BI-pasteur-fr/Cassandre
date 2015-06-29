4a63b46208412d55710b954ad2c3ec39af54a722
Hervé Ménager <herve.menager@gmail.com>
2015-06-29 14:25:13 +0200

    reformat code for web.js

--- web.js
+++ web.js
@@ -5,91 +5,111 @@ var _ = require('underscore');
 var cluster = require('cluster');
 var Measurement = require("./measurement").measurement;
 
-var router = function(app){
+var router = function(app) {
 
-app.get('/api/measurements/', function(req, res, next) {
-  Measurement.collection.find().toArray(
-    function (err, list) {
-      if (err) {
-        return res.status(500).send("Error with the database : " + err.message);
-      }
-      return res.status(200).send(list);
-  });
-});
+    app.get('/api/measurements/', function(req, res, next) {
+        Measurement.collection.find().toArray(
+            function(err, list) {
+                if (err) {
+                    return res.status(500).send("Error with the database : " + err.message);
+                }
+                return res.status(200).send(list);
+            });
+    });
 
-/* list all the measIds of the measurements collection */
-app.get('/api/measurements/list/', function(req, res, next) {
-  Measurement.collection.distinct('measId',
-    function (err, list) {
-      if (err) {
-        return res.status(500).send("Error with the database : " + err.message);
-      }
-      return res.status(200).send(list.map(function (measID) { return {"measID": measID} }));
-  });
-});
+    /* list all the measIds of the measurements collection */
+    app.get('/api/measurements/list/', function(req, res, next) {
+        Measurement.collection.distinct('measId',
+            function(err, list) {
+                if (err) {
+                    return res.status(500).send("Error with the database : " + err.message);
+                }
+                return res.status(200).send(list.map(function(measID) {
+                    return {
+                        "measID": measID
+                    }
+                }));
+            });
+    });
 
-/* list all the expIds for a given measId of the measurements collection */
-app.get('/api/measurements/:mId/exp/list/', function(req, res, next) {
-  Measurement.collection.distinct('expId',{'measId':req.params.mId},
-    function (err, list) {
-      if (err) {
-        return res.status(500).send("Error with the database : " + err.message);
-      }
-      return res.status(200).send(list.map(function (expID) { return {"expID": expID} }));
-  });
-});
+    /* list all the expIds for a given measId of the measurements collection */
+    app.get('/api/measurements/:mId/exp/list/', function(req, res, next) {
+        Measurement.collection.distinct('expId', {
+                'measId': req.params.mId
+            },
+            function(err, list) {
+                if (err) {
+                    return res.status(500).send("Error with the database : " + err.message);
+                }
+                return res.status(200).send(list.map(function(expID) {
+                    return {
+                        "expID": expID
+                    }
+                }));
+            });
+    });
 
-/* list all the geneIds for a given measId of the measurements collection */
-app.get('/api/measurements/:mId/gene/list/', function(req, res, next) {
-  Measurement.collection.distinct('geneId',{'measId':req.params.mId},
-    function (err, list) {
-      if (err) {
-        return res.status(500).send("Error with the database : " + err.message);
-      }
-      return res.status(200).send(list.map(function (geneID) { return {"geneID": geneID} }));
-  });
-});
+    /* list all the geneIds for a given measId of the measurements collection */
+    app.get('/api/measurements/:mId/gene/list/', function(req, res, next) {
+        Measurement.collection.distinct('geneId', {
+                'measId': req.params.mId
+            },
+            function(err, list) {
+                if (err) {
+                    return res.status(500).send("Error with the database : " + err.message);
+                }
+                return res.status(200).send(list.map(function(geneID) {
+                    return {
+                        "geneID": geneID
+                    }
+                }));
+            });
+    });
 
-/* list all the values for a given experiment in a given measurement */
-app.get('/api/measurements/:mId/exp/:expId', function(req, res, next) {
-  Measurement.collection.find({
-      'measId': req.params.mId,
-      'expId': req.params.expId
-    }).toArray(function (err, list) {
-      if (err) {
-        return res.status(500).send("Error with the database : " + err.message);
-      }
-      return res.status(200).send(list);
-  });
-});
+    /* list all the values for a given experiment in a given measurement */
+    app.get('/api/measurements/:mId/exp/:expId', function(req, res, next) {
+        Measurement.collection.find({
+            'measId': req.params.mId,
+            'expId': req.params.expId
+        }).toArray(function(err, list) {
+            if (err) {
+                return res.status(500).send("Error with the database : " + err.message);
+            }
+            return res.status(200).send(list);
+        });
+    });
 
-/* list all the values for a given gene in a given measurement */
-app.get('/api/measurements/:mId/gene/:geneId', function(req, res, next) {
-  Measurement.collection.find({
-      'measId': req.params.mId,
-      'geneId': req.params.geneId
-    }).toArray(function (err, list) {
-      if (err) {
-        return res.status(500).send("Error with the database : " + err.message);
-      }
-      return res.status(200).send(list);
-  });
-});
+    /* list all the values for a given gene in a given measurement */
+    app.get('/api/measurements/:mId/gene/:geneId', function(req, res, next) {
+        Measurement.collection.find({
+            'measId': req.params.mId,
+            'geneId': req.params.geneId
+        }).toArray(function(err, list) {
+            if (err) {
+                return res.status(500).send("Error with the database : " + err.message);
+            }
+            return res.status(200).send(list);
+        });
+    });
 
 };
 
-var WebServer = function(contacts){
-        var app = express();
-        app.use(bodyParser.urlencoded({ extended: false }));
-        app.use(bodyParser.json());
-        app.use(serveStatic('public', {'index': ['index.html']}));
-        server = app.listen(8080);
-        router(app);
-        console.log('Server listening at port 8080');
+var WebServer = function(contacts) {
+    var app = express();
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(bodyParser.json());
+    app.use(serveStatic('public', {
+        'index': ['index.html']
+    }));
+    server = app.listen(8080);
+    router(app);
+    console.log('Server listening at port 8080');
 }
 
-var launch = function(){
-        WebServer();
+var launch = function() {
+    WebServer();
 };
 
 module.exports = launch;
