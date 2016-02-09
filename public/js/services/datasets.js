0e7fd1dbfa73971d4a8d656c52b6aa5bdd06968d
Simon Malesys <simon.malesys@pasteur.fr>
2016-02-09 14:04:11 +0100

    Add the GNU GPL V3 License headers in most of the code files

--- public/js/services/datasets.js
+++ public/js/services/datasets.js
@@ -1,8 +1,30 @@
 /*
- * Angular service to access, set and share the data sets informations.
+ * Copyright (C) 2016 Simon Malesys - Institut Pasteur
+ *
+ * This file is part of Cassandre
+ *
+ * Cassandre is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cassandre is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
+// ============================================================================
+// ============================================================================
+
+/*
+ * Angular service to access, set and share the data sets informations.
+ *
+ */
 
 angular.module("cassandre").factory("datasets", ["datasetsHttp", "experiments", "genes", "stats", function datasetsFactory(datasetsHttp, experiments, genes, stats) {
 
@@ -95,7 +117,7 @@ angular.module("cassandre").factory("datasets", ["datasetsHttp", "experiments", 
             datasets.uploading = true;
             datasetsHttp.add(formData, function (response) {
                 var name = response.name;
-            
+
                 datasets.uploading = false;
                 alert("Dataset " + name + " successfully stored.");
 
